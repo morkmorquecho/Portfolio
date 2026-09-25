f0e0f29fee0a82672c377e7938abbec4413a71ae
morkmorquecho <mork.morquecho@gmail.com>
2026-09-25 12:03:01 -0600

    ad new project

--- lib/projects.ts
+++ lib/projects.ts
@@ -22,6 +22,16 @@ export const projects: Project[] = [
     image: "/projects/lorenza.png",
   },
   {
+    id: "island-finance",
+    title: "Island Finance · Personal finance web app",
+    description:
+      "Plataforma de gestión financiera personal que utiliza la metáfora de archipiélagos e islas para organizar tus finanzas: cada propósito financiero es un archipiélago y cada cuenta, una isla. Calcula intereses, registra movimientos, permite establecer metas de ahorro y visualiza activos y criptomonedas en tiempo real..",
+    tech: ["Django", "Vue", "PostgreSQL", "Redis"],
+    accent: "oklch(0.165 0.039 17.5)",
+    link: "https://islandfinance.cc",
+    image: "/projects/islandfinance.jfif",
+  },
+  {
     id: "glucobalance-multi",
     title: "GlucoBalance · Multiplataforma",
     description:
